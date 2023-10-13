9a693b469bd8eea4cc5f40380c999a1433027e25
Git-K2K <kristian20011@hotmail.com>
2023-10-13 08:42:18 +0200

    uplodaded files

--- script.js
+++ script.js
@@ -1,105 +1,91 @@
-const meny = [
-  {
-    namn: "FLÄSKFILÉPLANKA",
-    pris: 149,
-    beskrivning: "Duchesse mos, rödvinssky, bearnaise och stekta grönsaker",
-    typ: "fläsk",
-  },
-  {
-    namn: "BIFF RYDBERG",
-    pris: 175,
-    beskrivning:
-      "Tärnad oxfilé, potatis och gullök, serveras med dijonsenapskräm och rå äggula",
-    typ: "biff",
-  },
-  {
-    namn: "SPICY CHEESEBURGER",
-    pris: 155,
-    beskrivning:
-      "180gram högrevsburgare med sallad, tomat, picklade rödlök, cheddarost med jalapeños och barbecuesås i briochebröd med pommes och rökig ranch-aioli",
-    typ: "biff",
-  },
-  {
-    namn: "HALLOUMI BURGARE",
-    pris: 155,
-    beskrivning:
-      "Sallad, tomat, rödlök och guacamole i briochebröd med pommes och aiol",
-    typ: "veg",
-  },
-  {
-    namn: "FISH'N CHIPS",
-    pris: 149,
-    beskrivning:
-      '"Beer-batter" panerad torskrygg med dill-aioli, halstrad citron och pommes',
-    typ: "fisk",
-  },
-  {
-    namn: "HUSETS SCHNITZEL",
-    pris: 175,
-    beskrivning:
-      '200 gram välbankad schnitzel "Borås största" med citron, kapris, anjovis, kryddsmör och rödvinssky serveras med stekt potatis',
-    typ: "fläsk",
-  },
-  {
-    namn: "CAESARSALLAD",
-    pris: 139,
-    beskrivning:
-      "Varm kyckling, bacon, krutonger, parmesan samt caesardressing",
-    typ: "kyckling",
-  },
-  {
-    namn: "LAXSALLAD MED QUINOA",
-    pris: 159,
-    beskrivning:
-      "Grillad lax med quinoa, cocktailtomater, blandsallad, morot julienne, rödlök med en vinägrett",
-    typ: "fisk",
-  },
-  {
-    namn: "KRÄMIG KYCKLING PASTA",
-    pris: 169,
-    beskrivning: "Soltorkade tomater, basilika, grönsaker och parmesan",
-    typ: "kyckling",
-  },
-  {
-    namn: "MUSSLOR AL CAVA",
-    pris: 189,
-    beskrivning:
-      "Ångkokta musslor i en krämig sås gjord på cava, toppas med cream fraîche och jalapeños, serveras med pommes och aioli",
-    typ: "fisk",
-  },
-  {
-    namn: "GRILLAD OXFILE",
-    pris: 259,
-    beskrivning:
-      "200 gram oxfilé, rödvinssås, frästa grönsaker serveras med potatisgratäng (går att få med pommes och bearnaise) GRILLED BEEF TENDERLOIN ",
-    typ: "biff",
-  },
-];
-const kycklingRätter = meny.filter(
-  (kycklingRätter) => kycklingRätter.typ === ("kyckling", "biff")
-);
+import meny from "./meny.json" assert { type: "json" };
+import enmeny from "./enmeny.json" assert { type: "json" };
 
-const biffRätter = meny.filter((biff) => biff.typ === "biff");
-const menyItems = document.getElementsByClassName("meny-items")[0];
 
-kycklingRätter.forEach((maträtt) => {
-  let itemHR = document.createElement("HR");
-  let itemH3 = document.createElement("h3");
-  let itemBeskr = document.createElement("p");
-  let itemPris = document.createElement("p");
-  let itemH3Text = document.createTextNode(maträtt.namn);
-  let itemBeskrText = document.createTextNode(maträtt.beskrivning);
-  let itemPrisText = document.createTextNode(`${maträtt.pris} kr`);
-  itemH3.appendChild(itemH3Text);
-  itemBeskr.appendChild(itemBeskrText);
-  itemPris.appendChild(itemPrisText);
+//Dessa variabler hämtar referenser till olika DOM-element på webbsidan. 
+//checkboxes hämtar alla checkbox-element, menyItems refererar till ett DOM-element med klassen 
+// "meny-items", sortDropdown och languageChanger hämtar dropdown-menyer.
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const menyItems = document.querySelector(".meny-items");
+const sortDropdown = document.getElementById('sort');
+const languageChanger = document.getElementById('language-select');
 
-  menyItems.appendChild(itemH3);
+//Detta är en funktion som uppdaterar innehållet i DOM-elementet med klassen "meny-items" baserat på vilka checkboxes som är incheckade
+function displayFilteredDishes() {
+menyItems.innerHTML = ""; 
 
-  menyItems.appendChild(itemBeskr);
 
-  menyItems.appendChild(itemPris);
-  menyItems.appendChild(itemHR);
+  const selectedTypes = Array.from(checkboxes)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.getAttribute("typ"));
+
+  const filtreradeRätter = meny.filter((maträtt) => {
+    if (selectedTypes.length === 0) {
+      return true; // Visa alla rätter om ingen checkbox är vald
+    } else {
+      return selectedTypes.includes(maträtt.typ);
+    }
+  });
+ 
+  const selectedSort = sortDropdown.value;
+
+// Sortering i ordning
+// If "lägst pris" är valt på comboboxen så ska sidan visa de billigaste rätterna först
+// else if "högst pris" är valt på comboboxen så ska sidan visa de dyraste rätterna först
+  if (selectedSort === "Lägst Pris") {
+    
+    filtreradeRätter.sort((a, b) => a.pris - b.pris);
+  } else if (selectedSort === "Högst Pris") {
+    
+    filtreradeRätter.sort((a, b) => b.pris - a.pris);
+  }
+
+  filtreradeRätter.forEach((maträtt) => {
+    let itemHR = document.createElement("HR");
+    let itemH3 = document.createElement("h3");
+    let itemal = document.createElement("al")
+    let itemBeskr = document.createElement("p");
+    let itemPris = document.createElement("p");
+    let itemH3Text = document.createTextNode(maträtt.namn);
+    let itemBeskrText = document.createTextNode(maträtt.beskrivning);
+    let itemPrisText = document.createTextNode(`${maträtt.pris} kr`);
+    itemH3.appendChild(itemH3Text);
+    itemBeskr.appendChild(itemBeskrText);
+    itemPris.appendChild(itemPrisText);
+
+    menyItems.appendChild(itemH3);
+    menyItems.appendChild(itemBeskr);
+    menyItems.appendChild(itemPris);
+    menyItems.appendChild(itemHR);
+    menyItems.appendChild(itemal);
+  });
+}
+
+
+
+// Dessa eventlyssnare registrerar funktionen displayFilteredDishes att köras varje gång en checkbox ändras eller om det sker ändring i comboboxen.
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", displayFilteredDishes);
 });
 
-console.log(biffRätter);
+sortDropdown.addEventListener("change", displayFilteredDishes);
+
+// När sidan laddar visas alla rätter med hjälp av funktionen 
+displayFilteredDishes();
+
+
+
+// Funktionen ser till att alla checkboxes,comboboxes,språk återställs till standard eftersom att "checkbox.checked är false"
+
+function resetFilters() {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+  
+  sortDropdown.value = "Standard";
+  languageChanger.value = "svenska";
+  
+  displayFilteredDishes();
+}
+window.resetFilters = resetFilters; 
+
